19b1b3dd1e97182068e57316632f71658ea3420a
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-19 11:07:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HellApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tom";     //определяем переменную и инициализируем ее
+
+            Console.WriteLine(name);    //Tom
+
+            name = "Bob";    //меняем значение переменной 
+            Console.WriteLine(name);   //Bob
+
+            Console.Read();
+            
+        }
+    }
+}
